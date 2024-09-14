5e2e688dea6ed6dcb9706e8d75044a9e1da3ecc3
Prasad790-sys <psa19974@gmail.com>
2024-09-14 21:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function mincost(arr)
 { 
+	let c=0;
+	arr.sort((a,b)=>a-b);
+	while(arr.length>1){
+		let f=arr.shift();
+		let s=arr.shif();
+		c+=f+s;
+		arr.push(f+s);
+		arr.sort((a,b)=>a-b);
+	}
 //write your code here
+	return c;
 // return the min cost
   
 }
